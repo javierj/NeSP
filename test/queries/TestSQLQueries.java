81ab86f278c884148abcf51efd5a42ef1d36d385
JoinHearthstone <joinhearthstone@outlook.es>
2016-11-22 13:10:59 +0100

    Using Freemaker template engine

---
+++ test/queries/TestSQLQueries.java
@@ -0,0 +1,19 @@
+package queries;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Tsting soem wuries for extracing information from EAP file.
+ * @author Javier
+ *
+ */
+public class TestSQLQueries {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
